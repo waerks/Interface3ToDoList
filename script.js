0ddced1c5b6aec53e2996b4458fbc2304a5f11e5
Anaïs <a.aerts@interface3.be>
2024-06-18 13:34:17 +0200

    Push to GitHub

---
+++ script.js
@@ -0,0 +1,27 @@
+const userInput = document.getElementById('userInput');
+const btnAjout = document.querySelector('.btnAjout');
+const toDoList = document.querySelector('.list ul');
+
+
+
+btnAjout.addEventListener('click', (event) => {
+    userToDo(event);
+})
+
+function userToDo(){
+    const elemList = document.createElement('li');
+
+    const btnRemove = document.createElement('button');
+
+    btnRemove.innerText = "🗑️";
+    btnRemove.classList.add('btnRemove');
+
+    btnRemove.addEventListener('click', (event) => {
+        elemList.remove();
+    })
+
+    elemList.innerHTML = userInput.value + " ";
+    elemList.append(btnRemove);
+    
+    toDoList.append(elemList);
+}
